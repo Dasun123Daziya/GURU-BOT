29a71e7a6f1890428b17820243deead93b12babd
Dasun123Daziya <133541716+Dasun123Daziya@users.noreply.github.com>
2023-05-16 18:59:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'DULMI', true], 
+  ['94762084040', 'GURU', true],
+  ['94762084040', 'DULMI', true], 
   [''] 
 ] //Number of owners
 
